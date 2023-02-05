aafd4c3edfff42daecae6760e91d9c7650c1db9d
Santiago <sagos0919@gmail.com>
2023-02-05 22:52:47 +0000

    jumbo tron created and some files and folder added

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,28 @@
 import Navbar from './components/header/Navbar';
+import Jumbotron from './components/header/Jumbotron';
+import About from './components/pages/About';
 
+// # Importing images and SVGs
+import {
+	HTML5Logo,
+	CSSLogo,
+	JSLogo,
+	GITLogo,
+	GitHubLogo,
+	JQueryLogo,
+	BootstrapLogo,
+	NodejsLogo,
+} from './assets/svg/export';
 function App() {
 	return (
 		<>
-			<Navbar />
+			<header>
+				<Navbar />
+				<Jumbotron />
+			</header>
+			<main>
+				<About />
+			</main>
 		</>
 	);
 }
--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,15 +1,12 @@
-// Importing the libraries
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// Creating a reference to the #root div
-const el = document.getElementById('root');
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// Telling React to take control of the #root element
-const root = ReactDOM.createRoot(el);
-
-// Showing the component on the browser
-root.render(<App />);
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
