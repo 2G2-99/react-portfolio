df9729cd3a9e83a977a03ad7df2056f82c7f60be
Santiago <sagos0919@gmail.com>
2023-02-06 22:24:04 +0000

    main branch updated with last additions and changes

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,44 +1,24 @@
-import Navbar from './components/header/Navbar';
-import Jumbotron from './components/header/Jumbotron';
-import About from './components/pages/About';
-import AboutCard from './components/main/AboutCard';
-import AboutMe from './components/main/AboutMe';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-// # Importing images and SVGs
-import {
-	HTML5Logo,
-	CSSLogo,
-	JSLogo,
-	GITLogo,
-	GitHubLogo,
-	JQueryLogo,
-	BootstrapLogo,
-	NodejsLogo,
-} from './assets/svg/export';
-import picture from './assets/images/png-transparent-default-avatar-thumbnail.png';
+import Navbar from './components/header/Navbar';
+import Wrapper from './components/Wrapper/Wrapper';
+import { Home, About, Projects, Contact } from './pages/export';
 
 function App() {
 	return (
 		<>
-			<header>
-				<Navbar />
-				<Jumbotron />
-			</header>
-			<main>
-				<About>
-					<AboutCard image={picture}>
-						<AboutMe
-							content="Hello! I'm attending a web development boot camp with the
-									intention to become a full stack web developer."
-						/>
-						<AboutMe
-							content="There has always been a huge gap about the use and knowledge
-									of the technology we use every day. And there is much to
-									discover and create with all the tools we have now."
-						/>
-					</AboutCard>
-				</About>
-			</main>
+			<Router>
+				<Navbar>
+					<Wrapper>
+						<Routes>
+							<Route path="/" />
+							<Route path="/about" />
+							<Route path="/projects" />
+							<Route path="/contact" />
+						</Routes>
+					</Wrapper>
+				</Navbar>
+			</Router>
 		</>
 	);
 }
