13f15c89b98285d8a5b500de8433572199acadc2
Santiago <sagos0919@gmail.com>
2023-02-10 12:46:43 +0000

    Card component created

--- portfolio/src/components/export.js
+++ portfolio/src/components/export.js
@@ -4,10 +4,11 @@ import Wrapper from './Wrapper/index';
 import Container from './main/Container/index';
 import Row from './main/Row/index';
 import Column from './main/Column/index';
+import Card from './main/Card/index';
 
 // HEADER COMPONENTS
 import Hero from './header/Hero/index';
 import Navbar from './header/Navbar/index';
 
 // # Exporting components
-export { Wrapper, Container, Row, Column, Hero, Navbar };
+export { Wrapper, Container, Row, Column, Card, Hero, Navbar };
