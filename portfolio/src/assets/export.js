db2e3b47b0ff41390ae32573d97f2cda002aefc4
Santiago <sagos0919@gmail.com>
2023-02-11 21:38:00 +0000

    Some assets have been moved to the public folder inside of static_assets

--- portfolio/src/assets/export.js
+++ portfolio/src/assets/export.js
@@ -9,9 +9,6 @@ import BootstrapLogo from './svg/bootstrap-svgrepo-com.svg';
 import NodejsLogo from './svg/node-js-svgrepo-com.svg';
 import GoTo from './svg/internet-www-svgrepo-com.svg';
 
-// # Importing images
-import default_avatar from './images/png-transparent-default-avatar-thumbnail.png';
-
 // # Exporting all SVGs together for an easier importing when needed
 export {
 	HTML5Logo,
@@ -23,5 +20,4 @@ export {
 	BootstrapLogo,
 	NodejsLogo,
 	GoTo,
-	default_avatar,
 };
