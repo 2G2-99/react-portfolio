0b4b49e169b8568380eb0c27530d2831e72553f0
Santiago <sagos0919@gmail.com>
2023-02-11 02:13:47 +0000

    Card component created and added to the Projects page

--- portfolio/src/components/main/Card/index.js
+++ portfolio/src/components/main/Card/index.js
@@ -1,14 +1,57 @@
-const Card = ({ type, title, text }) => {
+const Card = ({
+	id,
+	title,
+	picture,
+	description,
+	technologies,
+	repository,
+	deployed,
+}) => {
 	const checkContentOf = content => {
 		if (!content) {
 			return null;
 		}
 	};
+
+	const generateBadgesOf = technologies => {
+		technologies.map(technology => {
+			return (
+				<>
+					<li key={id} className="list-group-item">
+						<img className="tech-icons" src={technology} alt="#" />
+					</li>
+				</>
+			);
+		});
+	};
+
+	const generateButtonOf = site => {
+		return (
+			<>
+				<button type="button" class={`btn btn-secondary btn-${site}`}>
+					<img src={site} alt={`Link to ${site}`} />
+				</button>
+			</>
+		);
+	};
+
 	return (
-		<div className={`card card-${type}`}>
-			<div className="card-body">
-				<div className={`card-${title}`}>{checkContentOf(title)}</div>
-				<div className={`card-${text}`}>{checkContentOf(text)}</div>
+		<div key={id} class={`card card-${title}`}>
+			<h5 class="card-title">{checkContentOf(title)}</h5>
+			<img
+				src={checkContentOf(picture)}
+				class="card-img-top"
+				alt={`Screenshot of ${title}`}
+			/>
+			<div class="card-body">
+				<p class="card-text">{checkContentOf(description)}</p>
+				<ul className="list-group list-group-horizontal">
+					{generateBadgesOf(technologies)}
+				</ul>
+				<div class="btn-group" role="group" aria-label="Basic example">
+					{generateButtonOf(repository)}
+					{generateButtonOf(deployed)}
+				</div>
 			</div>
 		</div>
 	);
--- portfolio/src/pages/Projects.js
+++ portfolio/src/pages/Projects.js
@@ -1,3 +1,34 @@
-const Projects = () => {};
+// # IMPORTS
+// COMPONENTS
+import { Container, Row, Column, Card } from '../components/export';
+
+// PROJECTS JSON
+import listOfProjects from '../listOfProjects.json';
+
+const Projects = () => {
+	return (
+		<>
+			<Container>
+				<Row>
+					<Column className={'col-12 col-md-6'}>
+						{listOfProjects.map(project => {
+							return (
+								<Card
+									id={project.id}
+									title={project.title}
+									picture={project.picture}
+									description={project.description}
+									technologies={project.technologies}
+									repository={project.repository}
+									deployed={project.deployed}
+								/>
+							);
+						})}
+					</Column>
+				</Row>
+			</Container>
+		</>
+	);
+};
 
 export default Projects;
