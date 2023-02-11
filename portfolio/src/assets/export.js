5739a522edcc15f2c6e52a2c22e3d591ea13ec43
Santiago <sagos0919@gmail.com>
2023-02-11 01:03:47 +0000

    Projects information and technologies badges added

---
+++ portfolio/src/assets/export.js
@@ -0,0 +1,27 @@
+// # Imports of all SVGs
+import HTML5Logo from './svg/html-5-svgrepo-com.svg';
+import CSSLogo from './svg/css-3-svgrepo-com.svg';
+import JSLogo from './svg/javascript-svgrepo-com.svg';
+import GITLogo from './svg/git-svgrepo-com.svg';
+import GitHubLogo from './svg/github-svgrepo-com.svg';
+import JQueryLogo from './svg/jquery-svgrepo-com.svg';
+import BootstrapLogo from './svg/bootstrap-svgrepo-com.svg';
+import NodejsLogo from './svg/node-js-svgrepo-com.svg';
+import GoTo from './svg/internet-www-svgrepo-com.svg';
+
+// # Importing images
+import default_avatar from './images/png-transparent-default-avatar-thumbnail.png';
+
+// # Exporting all SVGs together for an easier importing when needed
+export {
+	HTML5Logo,
+	CSSLogo,
+	JSLogo,
+	GITLogo,
+	GitHubLogo,
+	JQueryLogo,
+	BootstrapLogo,
+	NodejsLogo,
+	GoTo,
+	default_avatar,
+};
